0b0e9c14a91a82658e81ae420dda75119c26c020
gabe <playwigs@gmail.com>
2022-10-08 19:05:37 -0600

    Added db.create_all to connect_db function.

--- models.py
+++ models.py
@@ -4,6 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+def connect_db(app):
+    """Connect to the database."""
+
+    db.app = app
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
 class User(db.Model):
     """User info."""
 
@@ -19,10 +27,4 @@ class User(db.Model):
         """Returns the full name of the user."""
 
         return f"{self.first_name} {self.last_name}"
-
-def connect_db(app):
-    """Connect to the database."""
-
-    db.app = app
-    db.init_app(app)
     
