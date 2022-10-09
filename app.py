35d6f2e5aaa76efbbc23184ac6e0d682de1977cd
gabe <playwigs@gmail.com>
2022-10-08 19:06:46 -0600

    Created users route to display users in the db.

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-"""Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+
+@app.route("/")
+def home():
+    """Redirect to users page."""
+
+    return redirect("/users")
+
+@app.route("/users")
+def users():
+    """Display list of users."""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+
+    return render_template("users.html", users=users)
