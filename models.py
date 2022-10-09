3308fc33ed460262dfa3537760112f4f4b7f20d4
gabe <playwigs@gmail.com>
2022-10-08 18:03:27 -0600

    Added User model and function to connect to the db.

--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User info."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text)
+
+    @property
+    def full_name(self):
+        """Returns the full name of the user."""
+
+        return f"{self.first_name} {self.last_name}"
+
+def connect_db(app):
+    """Connect to the database."""
+
+    db.app = app
+    db.init_app(app)
+    
